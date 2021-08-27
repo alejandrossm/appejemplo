7d48ea208801722b7371a9a7ef213f951c6b01a8
alejandrossm <alejandrossm@outlook.com>
2021-08-27 14:30:20 -0400

    Paginas y navegar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'pag2',
+    loadChildren: () => import('./pages/pag2/pag2.module').then( m => m.Pag2PageModule)
+  },
+  {
+    path: 'pag3',
+    loadChildren: () => import('./pages/pag3/pag3.module').then( m => m.Pag3PageModule)
   },
 ];
 
