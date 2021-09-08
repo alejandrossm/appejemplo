eee398d15104513a7a60d0430b9fc67daa51509e
alejandrossm <alejandrossm@outlook.com>
2021-09-07 22:06:31 -0300

    Merge sin conflictos

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
-import { PieComponent } from './pie/pie.component';
+
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { FooterComponent } from './footer/footer.component';
 
